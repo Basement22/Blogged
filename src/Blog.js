1c8361a7e9fadc391e35a8cb512e25a38b39826b
Muhammad Hamza <mhfsd232@gmail.com>
2020-09-30 06:54:17 -0700

    changes done of second page

--- src/Blog.js
+++ src/Blog.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
@@ -6,6 +7,7 @@ import Container from '@material-ui/core/Container';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
+
 import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
--- src/ReadBlog.js
+++ src/ReadBlog.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+import Header from './Header';
+import Footer from './Footer';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+    },
+    mainGrid: {
+        marginTop: theme.spacing(3),
     },
     paper: {
         padding: theme.spacing(2),
@@ -13,8 +22,17 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function FullWidthGrid({match}) {
+export default function FullWidthGrid({ match }) {
     const classes = useStyles();
+
+    const sections = [
+        { title: 'Home', url: '#' },
+        { title: 'Blogs', url: '#' },
+        { title: 'Published', url: '#' },
+        { title: 'About us', url: '#' },
+        { title: 'Contact us', url: '#' },
+        { title: 'FAQs', url: '#' },
+    ];
 
     useEffect(() => {
         fetchBlog()
@@ -32,14 +50,23 @@ export default function FullWidthGrid({match}) {
     }
 
     return (
-        <div className={classes.root}>
-            <Grid container >
-                <Grid item xs={12}>
-                    <h1>{blog.title}</h1>
-                    <div className="blog" dangerouslySetInnerHTML={{ __html: blog.body }} style={{ height: 'auto', textAlign: 'left', marginLeft: '10vw', width: '80%' }} />
-                    <div />
-                </Grid>
-            </Grid>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title="Blogged" sections={sections} />
+                <main>
+                    <div className={classes.root}>
+                        <Grid container >
+                            <Grid item xs={12}>
+                                <h1>{blog.title}</h1>
+                                <div className="blog" dangerouslySetInnerHTML={{ __html: blog.body }} style={{ height: 'auto', textAlign: 'left', marginLeft: '10vw', width: '80%' }} />
+                                <div />
+                            </Grid>
+                        </Grid>
+                    </div>
+                </main>
+            </Container>
+            <Footer title="Footer" description="Something here to give the footer a purpose!" />
+        </React.Fragment>
     );
 }
